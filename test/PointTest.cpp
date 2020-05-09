8bf54417e90114662a4c8b3a74ac55ffef352f37
Rodolfo Araujo <rodoufu@gmail.com>
2020-05-09 15:28:28 -0300

    Fix more points test.

--- test/PointTest.cpp
+++ test/PointTest.cpp
@@ -154,12 +154,81 @@ TEST(NearestElements, MorePoints) {
 		Point{.x = 2, .y = 0},
 		Point{.x = 0, .y = 8},
 		Point{.x = 7, .y = 0},
-		Point{.x = 1, .y = 9},
-		Point{.x = 0, .y = 9},
 		Point{.x = 2, .y = 8},
+		Point{.x = 0, .y = 9},
+		Point{.x = 1, .y = 9},
 	};
 
-	check6NearestPoints(center, points);
+	{
+		auto nearest = nearestPoints(center, points, 7);
+		EXPECT_EQ(7, nearest.size());
+		vector<Point> expected{
+			Point{.x = 7, .y = 0},
+			Point{.x = 6, .y = 0},
+			Point{.x = 5, .y = 0},
+			Point{.x = 0, .y = 4},
+			Point{.x = 0, .y = 3},
+			Point{.x = 2, .y = 0},
+			Point{.x = 1, .y = 0},
+		};
+		for (auto i = 0; i < nearest.size(); ++i) {
+			checkPointsEqual(expected[i], nearest[i]);
+		}
+	}
+	{
+		auto nearest = nearestPoints(center, points, 8);
+		EXPECT_EQ(8, nearest.size());
+		vector<Point> expected{
+			Point{.x = 0, .y = 8},
+			Point{.x = 7, .y = 0},
+			Point{.x = 6, .y = 0},
+			Point{.x = 5, .y = 0},
+			Point{.x = 0, .y = 4},
+			Point{.x = 0, .y = 3},
+			Point{.x = 2, .y = 0},
+			Point{.x = 1, .y = 0},
+		};
+		for (auto i = 0; i < nearest.size(); ++i) {
+			checkPointsEqual(expected[i], nearest[i]);
+		}
+	}
+	{
+		auto nearest = nearestPoints(center, points, 9);
+		EXPECT_EQ(9, nearest.size());
+		vector<Point> expected{
+			Point{.x = 2, .y = 8},
+			Point{.x = 0, .y = 8},
+			Point{.x = 7, .y = 0},
+			Point{.x = 6, .y = 0},
+			Point{.x = 5, .y = 0},
+			Point{.x = 0, .y = 4},
+			Point{.x = 0, .y = 3},
+			Point{.x = 2, .y = 0},
+			Point{.x = 1, .y = 0},
+		};
+		for (auto i = 0; i < nearest.size(); ++i) {
+			checkPointsEqual(expected[i], nearest[i]);
+		}
+	}
+	{
+		auto nearest = nearestPoints(center, points, 10);
+		EXPECT_EQ(10, nearest.size());
+		vector<Point> expected{
+			Point{.x = 0, .y = 9},
+			Point{.x = 2, .y = 8},
+			Point{.x = 0, .y = 8},
+			Point{.x = 7, .y = 0},
+			Point{.x = 6, .y = 0},
+			Point{.x = 5, .y = 0},
+			Point{.x = 0, .y = 4},
+			Point{.x = 0, .y = 3},
+			Point{.x = 2, .y = 0},
+			Point{.x = 1, .y = 0},
+		};
+		for (auto i = 0; i < nearest.size(); ++i) {
+			checkPointsEqual(expected[i], nearest[i]);
+		}
+	}
 }
 
 TEST(NearestElements, RepeatedPoints) {
@@ -175,7 +244,7 @@ TEST(NearestElements, RepeatedPoints) {
 		Point{.x = 5, .y = 0},
 	};
 
-	{
+ 	{
 		auto nearest = nearestPoints(center, points, 7);
 		EXPECT_EQ(7, nearest.size());
 		vector<Point> expected{
@@ -221,3 +290,65 @@ TEST(NearestElements, RepeatedPoints) {
 		}
 	}
 }
+
+/*
+TEST(NearestElements, DistinctPointsSameDistance) {
+	Point center{.x =  0, .y =  0};
+	vector<Point> points{
+		Point{.x = 2, .y = 0},
+		Point{.x = 0, .y = 3},
+		Point{.x = 1, .y = 0},
+		Point{.x = 0, .y = 1},
+		Point{.x = 6, .y = 0},
+		Point{.x = 0, .y = 4},
+		Point{.x = 0, .y = 5},
+		Point{.x = 5, .y = 0},
+	};
+
+	{
+		auto nearest = nearestPoints(center, points, 7);
+		EXPECT_EQ(7, nearest.size());
+		vector<Point> expected{
+			Point{.x = 0, .y = 5},
+			Point{.x = 5, .y = 0},
+			Point{.x = 0, .y = 4},
+			Point{.x = 0, .y = 3},
+			Point{.x = 2, .y = 0},
+			Point{.x = 0, .y = 1},
+			Point{.x = 1, .y = 0},
+		};
+		for (auto i = 0; i < nearest.size(); ++i) {
+			checkPointsEqual(expected[i], nearest[i]);
+		}
+	}
+	{
+		auto nearest = nearestPoints(center, points, 6);
+		EXPECT_EQ(6, nearest.size());
+		vector<Point> expected{
+			Point{.x = 0, .y = 5},
+			Point{.x = 0, .y = 4},
+			Point{.x = 0, .y = 3},
+			Point{.x = 2, .y = 0},
+			Point{.x = 0, .y = 1},
+			Point{.x = 1, .y = 0},
+		};
+		for (auto i = 0; i < nearest.size(); ++i) {
+			checkPointsEqual(expected[i], nearest[i]);
+		}
+	}
+	{
+		auto nearest = nearestPoints(center, points, 5);
+		EXPECT_EQ(5, nearest.size());
+		vector<Point> expected{
+			Point{.x = 0, .y = 4},
+			Point{.x = 0, .y = 3},
+			Point{.x = 2, .y = 0},
+			Point{.x = 0, .y = 1},
+			Point{.x = 1, .y = 0},
+		};
+		for (auto i = 0; i < nearest.size(); ++i) {
+			checkPointsEqual(expected[i], nearest[i]);
+		}
+	}
+}
+ */
